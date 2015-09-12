85fcd79577c04a8ca7555a16530c3033a17f213f
Thomas11235 <thomas.oerlemans99@gmail.com>
2015-09-12 18:01:46 +0200

    Added a way to set player #2

--- source/PlayerTank.hx
+++ source/PlayerTank.hx
@@ -11,8 +11,6 @@ class PlayerTank extends FlxSprite
 
 	private var isPlayer1:Bool
 	
-	
-	public function new(X:Float=0, Y:Float=0) 
 	public function new(X:Float=0, Y:Float=0, IsPlayer1:Bool) 
 	{
 		super(X, Y);
