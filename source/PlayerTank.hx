881937a85590fcb9672ca1a58ee6565642a39ac7
Thomas11235 <thomas.oerlemans99@gmail.com>
2015-09-12 18:00:27 +0200

    Added a way to set player

--- source/PlayerTank.hx
+++ source/PlayerTank.hx
@@ -9,12 +9,15 @@ import flixel.FlxSprite;
 class PlayerTank extends FlxSprite
 {
 
+	private var isPlayer1:Bool
 	
 	
 	public function new(X:Float=0, Y:Float=0) 
+	public function new(X:Float=0, Y:Float=0, IsPlayer1:Bool) 
 	{
 		super(X, Y);
 		
+		isPlayer1 = IsPlayer1;
 	}
 	
 }
