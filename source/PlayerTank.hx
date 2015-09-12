6ff7f9b84e8ebdb5f818d10402ed91753a17e3f1
Thomas11235 <thomas.oerlemans99@gmail.com>
2015-09-12 18:04:01 +0200

    Added different buttons per player

--- source/PlayerTank.hx
+++ source/PlayerTank.hx
@@ -9,6 +9,11 @@ import flixel.FlxSprite;
 class PlayerTank extends FlxSprite
 {
 
+	private var _forwardKey:String;
+	private var _backwardKey:String;
+	private var _rLeftKey:String;
+	private var _rRightKey:String;
+	private var _shootKey:String;
 	private var isPlayer1:Bool
 	
 	public function new(X:Float=0, Y:Float=0, IsPlayer1:Bool) 
@@ -16,6 +21,23 @@ class PlayerTank extends FlxSprite
 		super(X, Y);
 		
 		isPlayer1 = IsPlayer1;
+		if (isPlayer1)
+		{
+			_forwardKey = "W";
+			_backwardKey = "S";
+			_rLeftKey = "A";
+			_rRightKey = "D";
+			_shootKey = "SPACE";
+		}
+		else 
+		{
+			_forwardKey = "UP";
+			_backwardKey = "DOWN";
+			_rLeftKey = "LEFT";
+			_rRightKey = "RIGHT";
+			_shootKey = "CONTROL";
+		}
+		
 	}
 	
 }
