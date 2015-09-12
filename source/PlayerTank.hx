8464e1208d3c8845f84ade35cc8aba0412c64196
Thomas11235 <thomas.oerlemans99@gmail.com>
2015-09-12 17:17:01 +0200

    Added basic classes
    added the empty classes for the tanks and for the walls

---
+++ source/PlayerTank.hx
@@ -0,0 +1,20 @@
+package;
+
+import flixel.FlxSprite;
+
+/**
+ * ...
+ * @author ...
+ */
+class PlayerTank extends FlxSprite
+{
+
+	
+	
+	public function new(X:Float=0, Y:Float=0) 
+	{
+		super(X, Y);
+		
+	}
+	
+}
---
+++ source/Wall.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxSprite;
+
+/**
+ * ...
+ * @author ...
+ */
+class Wall extends FlxSprite
+{
+
+	public function new(X:Float=0, Y:Float=0) 
+	{
+		super(X, Y);
+		
+	}
+	
+}
