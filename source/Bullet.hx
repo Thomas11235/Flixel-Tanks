6507fb9440ee7a0af3d5345eb717052ae3181d9a
Thomas11235 <thomas.oerlemans99@gmail.com>
2015-09-13 13:53:02 +0200

    Made the bullet a orange square

--- source/Bullet.hx
+++ source/Bullet.hx
@@ -1,6 +1,9 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.util.FlxAngle;
 
 /**
  * ...
@@ -9,10 +12,14 @@ import flixel.FlxSprite;
 class Bullet extends FlxSprite
 {
 
-	public function new(X:Float=0, Y:Float=0) 
+	//Variables for appearance
+	private var _width:Int = 8;
+	private var _height:Int = 8;
+	
+	public function new(X:Float=0, Y:Float=0, tankAngle:Float) 
 	{
 		super(X, Y);
-		
+		makeGraphic( _width, _height, FlxColor.ORANGE);
 	}
 	
 }
