117dd8a7ba8872be773a0e4d2fb4e819c2ebb3ce
Thomas11235 <thomas.oerlemans99@gmail.com>
2015-09-14 18:59:47 +0200

    Adds the tanks at the correct location

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -41,14 +41,34 @@ class PlayState extends FlxState
 		_bullets = new FlxTypedGroup<Bullet>();
 		add(_bullets);
 		
-		player1 = new PlayerTank(true, 30, 30);
+		player1 = new PlayerTank(true);	
 		add(player1);
 		
-		player2 = new PlayerTank(false, 70, 70);
+		player2 = new PlayerTank(false);
 		add(player2);
+		
+		_map.loadEntities(placeEntities, "entities");
 		
 		
 	}
+	
+	private function placeEntities(entityName:String, entityData:Xml):Void
+	{
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		
+		if (entityName == "player1")
+		{
+			player1.x = x;
+			player1.y = y;
+		}
+		else if (entityName == "player2")
+		{
+			player2.x = x;
+			player2.y = y;
+		}
+	}
+	
 	
 	private function shootBullets():Void
 	{
